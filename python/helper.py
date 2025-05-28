466e2a55b39aad9f6826921480113b42035b917d
HaykTarkhanyan <46108131+HaykTarkhanyan@users.noreply.github.com>
2025-05-28 20:08:34 +0200

    Py, modules wip

---
+++ python/helper.py
@@ -0,0 +1,15 @@
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+password = "hndkahav"
+
+print(f"{__name__ = }")
+
+if __name__ == "__main__":
+    print(password)
+
+    for i in range(4):
+        print("*"*i)
---
+++ python/main.py
@@ -0,0 +1,20 @@
+# import helper # 1
+# from helper import square, cube, password # 2
+# from helper import * # 3
+# import helper as h # 4
+# from helper import square as s # 5
+from helper import square
+
+tiv = int(input("Մուտքագրեք թիվը: "))
+
+print(tiv)
+print(square(tiv))
+
+
+
+# print(helper.square(tiv)) # 1
+# print(square(tiv)) # 2
+# print(cube(tiv))
+# print(password)
+
+# print(h.cube(tiv)) # 4
