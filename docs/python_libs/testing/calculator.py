161b607da691ab79bd538e9a406309e98ae12f81
HaykTarkhanyan <46108131+HaykTarkhanyan@users.noreply.github.com>
2026-02-20 19:09:28 +0100

    render 2026-02-20 18:59

--- docs/python_libs/testing/calculator.py
+++ docs/python_libs/testing/calculator.py
@@ -1,9 +1,10 @@
 def add(a: int, b: int) -> int:
-    return a + b
+    return a + b 
 
 def div(a: int, b: int) -> float:
     if b == 0:
-        raise ValueError("b must be non-zero")
+        print("AAAAA")
+        # raise ValueError("b must be non-zero")
     return a / b
 
 # test_calculator # test
@@ -13,23 +14,25 @@ def div(a: int, b: int) -> float:
 def subtract(a: int, b: int) -> int:
     return a - b
 
+def a():
+    pass
 # -------------- Debugging ------------
-def is_prime(n: int) -> bool:
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+# def is_prime(n: int) -> bool:
+#     if n <= 1:
+#         return False
+#     for i in range(2, int(n**0.5) + 1):
+#         if n % i == 0:
+#             return False
+#     return True
 
 
-done = []
-for a in range(-1, 10):
-    if a > 0:
-        a = int(a)
-        print(f"Is {a} prime? {is_prime(a)}")
-        done.append(a)
-    else:
-        print("Enter a positive integer greater than 0.")
+# done = []
+# for a in range(-1, 10):
+#     if a > 0:
+#         a = int(a)
+#         print(f"Is {a} prime? {is_prime(a)}")
+#         done.append(a)
+#     else:
+#         print("Enter a positive integer greater than 0.")
 
-print("All numbers entered:", done)
+# print("All numbers entered:", done)
